d706d46a2160ca516397d72eeac8400ba37ffe3a
audriusxp <palm@inbox.lt>
2022-06-13 17:18:52 +0300

    atliktas uzduotis, bet turiu abejone ar viska teisingai supratau ko praso

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 window.onload = function () {
+    let zodis = "namas";
+    let zodis1 = "kosmonautas";
+    let zodis2 = "rutina";
+    let zodis3 = "dehidatacija";
+    
+    console.log(zodzioIlgis(zodis));
+    console.log(zodzioIlgis(zodis1));
+    console.log(zodzioIlgis(zodis2));
+    console.log(zodzioIlgis(zodis3));
+   
+   
     
 }
+function zodzioIlgis(text)
+{  if((text.length>10)){
+    return true;
+}
+else((text.length<10))
+return false;
+
+}
+    
+    
